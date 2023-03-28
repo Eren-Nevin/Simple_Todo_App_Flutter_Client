f449d20e137ee51e54c5d367e05ff1eed95f8b28
MVP <maverickvparker@gmail.com>
2023-03-28 03:19:47 +0300

    Modified build targets

--- lib/context.dart
+++ lib/context.dart
@@ -2,8 +2,9 @@ import 'package:flutter/foundation.dart';
 
 String getBaseUrl() {
   if (kReleaseMode) {
-    return "https://dinkedpawn.com:9999";
+    return "https://todo.dinkedpawn.com";
   } else {
-    return "http://localhost:9999";
+    return "https://todo.dinkedpawn.com";
+    // return "http://localhost:9999";
   }
 }
